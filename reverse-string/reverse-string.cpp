00391c3534d4568e357aeefbb0b679b4b2dcdb89
miracle-coding <84512918+miracle-coding@users.noreply.github.com>
2021-05-21 06:01:44 +0900

    Time: 32 ms (6.07%), Space: 23.3 MB (27.73%) - LeetHub

--- reverse-string/reverse-string.cpp
+++ reverse-string/reverse-string.cpp
@@ -3,9 +3,7 @@ public:
     void reverseString(vector<char>& s) {
         int left = 0, right = s.size()-1;
         while (left < right) {
-            char tmp = s[right];
-            s[right--] = s[left];
-            s[left++] = tmp;
+            swap(s[left++], s[right--]);
         }
     }
 };
