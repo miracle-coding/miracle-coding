c8dd481d665f7be90628065ced724183e08559f7
miracle-coding <84512918+miracle-coding@users.noreply.github.com>
2021-05-21 06:16:44 +0900

    Time: 32 ms (6.07%), Space: 23.3 MB (27.73%) - LeetHub

--- reverse-string/reverse-string.cpp
+++ reverse-string/reverse-string.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    void rec(int left, int right, vector<char>& s) {
-        if (left >= right) return;
-        swap(s[left], s[right]);
-        rec(left+1, right-1, s);
-    }
-    
     void reverseString(vector<char>& s) {
-        rec(0, s.size()-1, s);
+        int len = s.size();
+        if (len < 2) return;
+        int left = 0, right = len-1;
+        while (left < right)
+            swap(s[left++], s[right--]);
     }
 };
