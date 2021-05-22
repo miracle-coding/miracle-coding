9b8cc04f9343aa56c80376ec03f8606a9cbd2f7b
miracle-coding <84512918+miracle-coding@users.noreply.github.com>
2021-05-22 11:20:51 +0900

    Time: 40 ms (84.18%), Space: 7.1 MB (78.06%) - LeetHub

--- count-primes/count-primes.cpp
+++ count-primes/count-primes.cpp
@@ -2,23 +2,20 @@
 
 class Solution {
 public:
-    vector<bool> sieve;
-    
-    void e_sieve(ull n) {
-        sieve.resize(n+1, true);
+    int countPrimes(int n) {
+        if (n<=2) return 0;
+        
+        vector<bool> sieve(n+1, true);
         for (ull i=2 ; i*i<n ; ++i) {
             if (!sieve[i]) continue;
             for (ull j=i*i ; j<n ; j+=i) {
                 sieve[j] = false;
             }                
         }
-    }
-
-    int countPrimes(int n) {
-        e_sieve(n);
+        
         int cnt = 0;
         for (int i=2 ; i<n ; ++i) {
-            if(sieve[i]) ++cnt;
+            if (sieve[i]) ++cnt;
         }
         return cnt;
     }
