4f06a036103655b646bed6f6f4036d3bcd856187
miracle-coding <84512918+miracle-coding@users.noreply.github.com>
2021-05-23 10:25:03 +0900

    Time: 4 ms (91.02%), Space: 13.1 MB (36.40%) - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int sum = 0, diff = 0, pivot = prices[prices.size()-1];
+        int sum = 0;
         for (int i=prices.size()-1 ; i>0 ; --i) {            
-            diff = pivot - prices[i-1];
-            if (diff > 0) sum += diff;
-            pivot = prices[i-1];
+            if (prices[i] - prices[i-1] > 0)
+                sum += prices[i] - prices[i-1];
         }
         return sum;
     }
