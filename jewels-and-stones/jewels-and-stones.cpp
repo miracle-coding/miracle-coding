ffdcaf833b698237b87c584e515cf8cb00407786
miracle-coding <84512918+miracle-coding@users.noreply.github.com>
2021-05-28 23:16:01 +0900

    Time: 0 ms (100.00%), Space: 6.5 MB (20.56%) - LeetHub

--- jewels-and-stones/jewels-and-stones.cpp
+++ jewels-and-stones/jewels-and-stones.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         unordered_map<char, bool> umap;
-        for (auto c : jewels) {
-            umap[c] = true;
-        }
+        for (auto c : jewels) umap[c] = true;
         int cnt = 0;
-        for (auto c : stones) {
-            if (umap[c]) ++cnt;
-        }
+        for (auto c : stones) if (umap[c]) ++cnt;
         return cnt;
     }
 };
