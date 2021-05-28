cd89d42a0766c3d82eb99df5cd11a0be94dccc42
miracle-coding <84512918+miracle-coding@users.noreply.github.com>
2021-05-28 23:25:34 +0900

    Time: 388 ms (62.38%), Space: 152 MB (50.38%) - LeetHub

--- range-sum-query-mutable/range-sum-query-mutable.cpp
+++ range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -2,22 +2,20 @@ class NumArray {
 public:
     NumArray(vector<int>& nums) {
         N = nums.size();
+        arr.resize(N);
+        for (int i=0 ; i<N ; ++i)
+            arr[i] = nums[i];
+        
         int h = ceil(log2(N));
         int tree_size = 1 << (h+1);
         tree.resize(tree_size);
-        arr.resize(N);
-        for (int i=0 ; i<N ; ++i) {
-            arr[i] = nums[i];
-        }
-        
         init(0, 0, N-1);
     }
     
     int init(int node, int start, int end) {
         if (start == end) return tree[node] = arr[start];
-        int mid = (start + end) / 2;
-        return tree[node] = init(2*node+1, start, mid) +
-            init(2*node+2, mid+1, end);
+        int mid = (start+end)/2;
+        return tree[node] = init(2*node+1, start, mid) + init(2*node+2, mid+1, end);
     }
     
     void update(int index, int val) {
@@ -25,12 +23,12 @@ public:
         arr[index] = val;
         _update(0, 0, N-1, index, diff);
     }
-    
+
     void _update(int node, int start, int end, int index, int diff) {
         if (index < start || end < index) return;
         tree[node] += diff;
         if (start == end) return;
-        int mid = (start + end) / 2;
+        int mid = (start+end)/2;
         _update(2*node+1, start, mid, index, diff);
         _update(2*node+2, mid+1, end, index, diff);
     }
@@ -42,7 +40,7 @@ public:
     int _sum(int node, int start, int end, int qleft, int qright) {
         if (qright < start || end < qleft) return 0;
         if (qleft <= start && end <= qright) return tree[node];
-        int mid = (start + end) / 2;
+        int mid = (start+end)/2;
         return _sum(2*node+1, start, mid, qleft, qright) +
             _sum(2*node+2, mid+1, end, qleft, qright);
     }
