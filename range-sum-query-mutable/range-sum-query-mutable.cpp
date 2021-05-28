9e4ef4907f5c58cbb9c3ed4187b4b62b0875b220
miracle-coding <84512918+miracle-coding@users.noreply.github.com>
2021-05-28 23:32:41 +0900

    Time: 360 ms (93.98%), Space: 150.8 MB (71.62%) - LeetHub

--- range-sum-query-mutable/range-sum-query-mutable.cpp
+++ range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,53 +1,44 @@
 class NumArray {
 public:
     NumArray(vector<int>& nums) {
-        N = nums.size();
-        arr.resize(N);
-        for (int i=0 ; i<N ; ++i)
-            arr[i] = nums[i];
-        
-        int h = ceil(log2(N));
-        int tree_size = 1 << (h+1);
-        tree.resize(tree_size);
-        init(0, 0, N-1);
-    }
-    
-    int init(int node, int start, int end) {
-        if (start == end) return tree[node] = arr[start];
-        int mid = (start+end)/2;
-        return tree[node] = init(2*node+1, start, mid) + init(2*node+2, mid+1, end);
+        int N = nums.size();
+        arr.resize(N+1);
+        tree.resize(N+1);
+        for (int i=0 ; i<N ; ++i) {
+            arr[i+1] = nums[i];  
+            _update(i+1, arr[i+1]);
+        } 
     }
     
     void update(int index, int val) {
-        int diff = val - arr[index];
-        arr[index] = val;
-        _update(0, 0, N-1, index, diff);
+        int diff = val - arr[index+1];
+        arr[index+1] = val;
+        _update(index+1, diff);
     }
-
-    void _update(int node, int start, int end, int index, int diff) {
-        if (index < start || end < index) return;
-        tree[node] += diff;
-        if (start == end) return;
-        int mid = (start+end)/2;
-        _update(2*node+1, start, mid, index, diff);
-        _update(2*node+2, mid+1, end, index, diff);
+    
+    void _update(int i, int diff) {
+        while (i < arr.size()) {
+            tree[i] += diff;
+            i += (i & -i);
+        }
     }
     
     int sumRange(int left, int right) {
-        return _sum(0, 0, N-1, left, right);
+        ++right; ++left;
+        return _sum(right) - _sum(left-1);
     }
     
-    int _sum(int node, int start, int end, int qleft, int qright) {
-        if (qright < start || end < qleft) return 0;
-        if (qleft <= start && end <= qright) return tree[node];
-        int mid = (start+end)/2;
-        return _sum(2*node+1, start, mid, qleft, qright) +
-            _sum(2*node+2, mid+1, end, qleft, qright);
+    int _sum(int i) {
+        int ans = 0;
+        while (i) {
+            ans += tree[i];
+            i -= (i & -i);
+        }
+        return ans;
     }
     
     vector<int> arr;
     vector<int> tree;
-    int N;
 };
 
 /**
