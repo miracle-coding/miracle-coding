2949a931bfddc0c32917d0f1b9e5570891f92081
miracle-coding <84512918+miracle-coding@users.noreply.github.com>
2021-05-31 06:09:07 +0900

    Time: 1596 ms (5.23%), Space: 31.9 MB (5.03%) - LeetHub

--- longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
+    int longestConsecutive(vector<int>& nums) {
+        set<int> seq(nums.begin(), nums.end());
+        int longest = 0;
+        for (int& x : nums) {
+            if (seq.find(x-1) != seq.end()) continue;
+            int y = x + 1;
+            while (seq.find(y) != seq.end()) ++y;
+            longest = max(longest, y-x);
+        }
+        return longest;
+    }
+    
+    /*
     int get_parent(int x) {
         if (x == parent[x]) return x;
         return parent[x] = get_parent(parent[x]);
     }
-    
     void merge(int x, int y) {
         x = get_parent(x);
         y = get_parent(y);
@@ -24,7 +36,6 @@ public:
         if (longest_size < group[idx])
             longest_size = group[idx];
     }
-    
     int longestConsecutive(vector<int>& nums) {
         if (nums.empty()) return 0;
         if (1 == nums.size()) return 1;
@@ -37,15 +48,14 @@ public:
             if (parent.find(num+1) != parent.end() && group[num+1]) {
                 merge(num, num+1);
             }
-            
             if (parent.find(num-1) != parent.end()) {
                 merge(num-1, num);
             }
         }
         return longest_size;
     }
-    
     unordered_map<int, int> parent;
     unordered_map<int, int> group;
     int longest_size;
+    */
 };
